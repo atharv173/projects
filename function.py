966e7752dfc651fa22d6ba0ebadaead3b1b73ab4
atharv173 <varshakadam156@gmail.com>
2026-04-29 08:50:21 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,31 @@
+def z(v,x):    # defination of function  # v,x are parameters
+    y = v + x  #
+    print(y)
+
+z(4,5)         # function call           # function argument
+z(6,7)
+
+#def → used to create a function
+#z → function name
+#(v, x) → parameters (inputs)
+
+name=input("enter a name")
+def intro(name):
+    print(name)
+
+intro(name)
+
+
+def well_wishes():
+    print("hello!! how are you")
+
+well_wishes()
+
+#-------------------------------------------------------------------------
+
+def add(c,d):
+    print(c+d)
+
+add(5,6)
+add(34,78)
+
