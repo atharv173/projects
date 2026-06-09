1ef3a7752e834cb3e00b722f49e638d96d2c5d09
atharv173 <varshakadam156@gmail.com>
2026-06-09 08:35:10 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+grade_book = {"name1":"kartik","score1":92,
+           "name2":"raj","score2":89,
+           "name3":"sam","score3":87,
+           "name4":"tom","score4":97,
+           "name5":"shourya","score5":96
+}
+
+
+class_average = 92,89,87,97,96/5
+print(grade_book["name4"])
+print(grade_book["name1"])
